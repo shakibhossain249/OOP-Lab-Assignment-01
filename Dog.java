1a684a087a1e87301b2209a81172bfe92447aa46
shakibhossain249 <shakib21776@gmail.com>
2025-11-08 20:23:22 +0600

    Add Dog class with constructor and display method

--- Dog.java
+++ Dog.java
@@ -1,1 +1,30 @@
 
+package com.mycompany.dog;
+
+public class Dog {
+
+    
+    private String name;
+    private String color;
+
+    
+    public Dog(String name, String color) {
+        this.name = name;  
+        this.color = color;
+    }
+
+ 
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Color: " + color);
+    }
+
+    
+    public static void main(String[] args) {
+        
+        Dog dog1 = new Dog("Buddy", "Brown");
+
+        
+        dog1.display();
+    }
+}
