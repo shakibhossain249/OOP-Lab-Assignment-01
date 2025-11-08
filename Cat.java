8d0d0f379a93ce95f7a87bcdf90c21fc501c5ca3
shakibhossain249 <shakib21776@gmail.com>
2025-11-08 20:21:40 +0600

    Update Cat.java

--- Cat.java
+++ Cat.java
@@ -1,1 +1,26 @@
 
+package com.mycompany.cat;
+
+public class Cat {
+    
+    String name;
+    int age;
+
+    
+    public Cat() {
+        name = "TINTIN";
+        age = 4;
+    }
+
+    
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    
+    public static void main(String[] args) {
+        Cat cat1 = new Cat(); 
+        cat1.display();
+    }
+}
