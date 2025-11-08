aa757d68b29b58c6d0915f06829be4fea768b98b
shakibhossain249 <shakib21776@gmail.com>
2025-11-08 20:27:04 +0600

    Update HRManager.java

--- HRManager.java
+++ HRManager.java
@@ -1,1 +1,28 @@
 
+class Employee {
+    void work() {
+        System.out.println("Employee is working");
+    }
+
+    double getSalary() {
+        return 25000;
+    }
+}
+
+class HRManager extends Employee {
+    @Override
+    void work() {
+        System.out.println("HR Manager is managing employees");
+    }
+
+    void addEmployee() {
+        System.out.println("Adding a new employee");
+    }
+
+    public static void main(String[] args) {
+        HRManager hr = new HRManager();
+        hr.work();
+        hr.addEmployee();
+        System.out.println("Salary: " + hr.getSalary());
+    }
+}
